54edf2c9e703c1ef70ac4d8ea4a674473e11c24e
Zaher Yamak <zaher.yamak@esigelec.fr>
2026-01-04 23:02:45 +0100

    modif flask

--- app2.py
+++ app2.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Hello, DevOps!"
+    return "Hello, world!"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
